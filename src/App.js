8ce5666c0bb58f9a9636139258f3412ee5d0bfcb
Tristan Berezowski <tristanberezowski@gmail.com>
2020-08-19 16:57:39 -0700

    TicTacToe board and ai added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Client } from 'boardgame.io/react';
 import { TicTacToe } from './Game';
+import { TicTacToeBoard} from './Board'
 
-const App = Client({ game: TicTacToe });
+const App = Client({ game: TicTacToe, board: TicTacToeBoard });
 
 export default App;
